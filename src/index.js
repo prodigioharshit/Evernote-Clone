13d4f9053f2cd74842db78995f25e54b1866f4d0
prodigioharshit <harshit19051915@gmail.com>
2020-06-28 17:57:47 +0530

    Set up index.js for firebase and react-app

--- src/index.js
+++ src/index.js
@@ -4,11 +4,29 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+const firebase = require('firebase');
+require('firebase/firestore');
+
+ // Your web app's Firebase configuration
+ const firebaseConfig = {
+  apiKey: "AIzaSyCyw40F5N1T6m-YNCO5hB8B6VsxX6MhEIU",
+  authDomain: "evernote-clone-e46c0.firebaseapp.com",
+  databaseURL: "https://evernote-clone-e46c0.firebaseio.com",
+  projectId: "evernote-clone-e46c0",
+  storageBucket: "evernote-clone-e46c0.appspot.com",
+  messagingSenderId: "1005616359573",
+  appId: "1:1005616359573:web:3e82677baad80ad1de139c",
+  measurementId: "G-KYG4JCXM3S"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('evernote-container')
 );
 
 // If you want your app to work offline and load faster, you can change
